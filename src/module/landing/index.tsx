ba86eb55fde94ae3e2a36d0a96d7666fcf4d460e
mhmdhaekal <muhammad.haekal22@ui.ac.id>
2023-02-11 19:26:12 +0700

    found bug in modal

--- src/module/landing/index.tsx
+++ src/module/landing/index.tsx
@@ -1,31 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { Tooltips } from 'compfest-silicon';
+import { Button, Modal } from 'compfest-silicon';
+
+
 
 export const Landing: React.FC = () => {
 
     const [data, setData] = useState<boolean>(false);
+
+    
     
     return (
         <div>
 
-            <h1 style={{
-                color: 'white'
-            }}>Test</h1>
-
-            <h1 style={{
-                color: 'white'
-            }}>Test</h1>
-            
-
-            <h1 style={{
-                color: 'white'
-            }}>Test</h1>
-            
-            <Tooltips
-                id='tooltip'
-                text='Tooltip'
-                children={<div>Tooltip</div>}
-            />            
+        <Button onClick={() => setData(true)}>Click Me</Button>
+        <Modal
+        title='Modal Title'
+        acceptButton='Accept'
+        cancelButton='Cancel'
+        onClose={() => setData(false)}
+        isDisplayed={data}
+        />
+          
         </div>
     )
 }
